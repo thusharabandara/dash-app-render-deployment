5e8ee77285c075c26b8718a1e6f47b7efe9feb00
thusharabandara <thushararanjana@gmail.com>
2023-05-05 09:30:14 +0530

    import missing library: plotly

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import plotly.express as px
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
